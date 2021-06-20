522409f9714322b29613b1d591c51b06325acc94
Jonas Surmann <jonas.p.surmann@gmail.com>
2021-06-20 21:40:28 +0800

    Vision ueberarbeitet

--- tmp/1rhmuqyegwrps.ypm7k0yseszr.php
+++ tmp/1rhmuqyegwrps.ypm7k0yseszr.php
@@ -9,7 +9,28 @@
 		<div class="row content">
 			<div class="col-lg-6" data-aos="fade-up" data-aos-delay="150">
 				<p style="text-align:justify">
-					Eine App mit der alle auf der alle auf der Defichain handelbaren Aktien, Krytow&auml;hrungen und sonstigen Assets rund um die Uhr, zensurresistent gehandelt werden k&ouml;nnen, sodass der Nutzer immer die volle Kontrolle &uuml;ber seine Anlagen hat. Einfach und umkompliziert.</p>
+					Wir tragen dazu bei maximale Freiheit zu erlangen, indem wir allen Menschen die Verantwortung
+					&uuml;ber ihre eigenen Finanzen &uuml;tragen, sodass niemand mehr auf Drittanbieter wie Banken, Versicherungen
+					oder Finanzdienstleister sowie auf FIAT-W&auml;hrungen angewiesen ist. Das Recht auf Selbstbestimmung &uuml;ber
+					die eigenen Finanzen ist ein elementarer Bestandteil der Freiheit eines jeden Individuums.
+					Decentralized Finance (DeFi) kann diese Freiheit gewaehrleisten und besch&uuml;tzen durch dezentrale Finanzinstrumente,
+					die zensurresistent, enteignungssicher und zeitlich unbegrenzt von jedem individuell verwahrt als auch gehandelt
+					werden k&ouml;nnen.
+					<br />
+					<br />
+					Bisher sind etliche Schritte notwendig, um in DeFi Assets zu investieren bzw. mit diesen zu handeln, was viele
+					Menschen davon abh&auml;lt dieses dezentrale &Ouml;kosystem zu nutzen. Insbesondere die limitierte Nutzerfreundlichkeit
+					und die fehlende Integration von &uuml;blichen Zahlungsmethoden sind Barrieren, die potenzielle Nutzer von Defichain
+					fernh&auml;lt.
+					<br />
+					<br />
+					Die Decentralized Finance Exchange tritt an die einfachste und intuivste Applikation zu bauen, um es Nutzern
+					zu erm&ouml;glichen rund um die Uhr in alle DeFi Assets oder andere Kryptow&auml;hrungen zu investieren,
+					ohne je die Kontrolle und Verf&uuml;gungsgewalt &uuml;ber diese Assets zu verlieren.
+					Dazu werden alle &uuml;blichen Zahlungsmethoden integriert, sodass ein Handel mit DeFi Assets so
+					selbstverst&auml;ndlich wie jeder andere Kauf oder Verkauf eines Guts ist. Die Decentralized Finance Exchange
+					erm&ouml;glicht es Menschen unmittelbar und unkompliziert ihre FIAT-W&auml;hrungen in DeFi zu &uuml;tragen.
+					</p>
 			</div>
 			<div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-up" data-aos-delay="300">
 				<p style="text-align:justify">
--- ui/en/about/sections/about.htm
+++ ui/en/about/sections/about.htm
@@ -9,7 +9,25 @@
 		<div class="row content">
 			<div class="col-lg-6" data-aos="fade-up" data-aos-delay="150">
 				<p style="text-align:justify">
-					An app with which all shares, crytocurrencies and other assets that can be traded on Defichain can be traded around the clock, resistant to censorship, and where the user always has full control over his investments. No one can expropriate you.</p>
+					We are helping to achieve maximum freedom by giving anyone responsibility for their own finances, so that no one
+					is dependent on third-party providers such as banks, insurance companies or financial service providers, or on
+					FIAT-currencies. The right to self-determination over one's own finances is a fundamental part of every individual's
+					freedom. Decentralized Finance (DeFi) can guarantee and protect this freedom through decentralized financial
+					instruments that are censorship-resistant, expropriation-proof and can be held and traded by anyone individually
+					for an unlimited period of time.
+					<br />
+					<br />
+					So far, quite a few steps are required to invest in or trade DeFi assets which prevents many people from using
+					this decentralized ecosystem. In particular, the limited user experience and lack of integration of common payment
+					systems are barriers that keep potential users away from DeFi.
+					<br />
+					<br />
+					The Decentralized Finance Exchange is stepping up to build the simplest and most intuitive application to allow users
+					to invest in any DeFi assets or other cryptocurrencies 24/7 without ever losing control and disposition of those assets.
+					To this end, all standard payment systems will be integrated, so that trading DeFi Assets is as natural as any other
+					purchase or sale of an asset. The Decentralized Finance Exchange allows people to immediately and easily swap their
+					FIAT-currencies into DeFi.
+					</p>
 			</div>
 			<div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-up" data-aos-delay="300">
 				<p style="text-align:justify">
