aaa70f8850c956c7d2fe200c75280e68f439be60
Jonas Surmann <jonas.p.surmann@gmail.com>
2021-06-20 14:13:39 +0800

    Added About section to Header

--- tmp/1rhmuqyegwrps.33xe7187j6040.php
+++ tmp/1rhmuqyegwrps.33xe7187j6040.php
@@ -1,6 +1,5 @@
 <?php echo $this->render('en/home/sections/startpage.htm',NULL,get_defined_vars(),0); ?>
 <?php echo $this->render('en/home/sections/about.htm',NULL,get_defined_vars(),0); ?>
-<?php echo $this->render('en/home/sections/vision.htm',NULL,get_defined_vars(),0); ?>
 <?php echo $this->render('en/home/sections/function.htm',NULL,get_defined_vars(),0); ?>
 <?php echo $this->render('en/home/sections/roadmap.htm',NULL,get_defined_vars(),0); ?>
 <?php echo $this->render('en/home/sections/proofoffunds.htm',NULL,get_defined_vars(),0); ?>
--- tmp/1rhmuqyegwrps.3vllyran5mm8o.php
+++ tmp/1rhmuqyegwrps.3vllyran5mm8o.php
@@ -1,21 +1,21 @@
 <section class="about" id="about">
 	<div class="container">
+		<br/>
+		<br/>
 		<div class="section-title" data-aos="fade-up">
 			<h2>
 				Our Vision</h2>
 		</div>
 		<div class="row content">
 			<div class="col-lg-6" data-aos="fade-up" data-aos-delay="150">
-
+				<p style="text-align:justify">
+					An app with which all shares, crytocurrencies and other assets that can be traded on Defichain can be traded around the clock, resistant to censorship, and where the user always has full control over his investments. No one can expropriate you.</p>
+			</div>
+			<div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-up" data-aos-delay="300">
+				<p style="text-align:justify">
+					<img alt="" src="/assets/img/mobile-app%402x.png" style="width: 404px; height: 192px;" /></p>
+			</div>
 		</div>
 	</div>
-	<br />
-	<div class="row content">
-		<div class="col-lg-6" data-aos="fade-up" data-aos-delay="150">
-
-
-	</div>
-</div>
-
 </section>
 <br />
