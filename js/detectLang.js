f7204a3d7b406f47dfe5344c6bc7cbb17ee3ddcb
Jonas Surmann <jonas.p.surmann@gmail.com>
2021-11-09 16:00:18 +0200

    Fix detectLang Script

--- js/detectLang.js
+++ js/detectLang.js
@@ -1,6 +1,8 @@
 var userLang = navigator.language || navigator.userLanguage;
 //alert ("The language is: " + userLang);
 console.log(userLang);
-if (userLang !== "de") {
-  window.location.href = "en.html";
+if (userLang === 'de') {
+  window.location.href = "/";
+} else {
+    window.location.href = "en.html";
 }
