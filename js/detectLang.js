d75b8a6d357567302d4e9821967fb4fe6292e229
Jonas Surmann <jonas.p.surmann@gmail.com>
2021-11-09 16:18:25 +0200

    fix lang

--- js/detectLang.js
+++ js/detectLang.js
@@ -1,4 +1,5 @@
-var userLang = navigator.language || navigator.userLanguage;
+//var userLang = navigator.language || navigator.userLanguage;
+var userLang = window.navigator.userLanguage || window.navigator.language;
 //alert ("The language is: " + userLang);
 console.log(userLang);
 if (userLang === 'de') {
